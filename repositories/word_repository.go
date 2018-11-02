eff53e6681ab4b08fbae6bd4b559cf58957dcf58
xiangguang <xiangguang@staff.weibo.com>
2018-11-02 15:40:02 +0800

    updated

--- repositories/word_repository.go
+++ repositories/word_repository.go
@@ -9,10 +9,10 @@ import (
 
 
 type WordRepository interface {
-	Select(where string)(w datamodels.Word,found bool)
-	SelectMany(where, group_by, order_by string, page, size int) (results []datamodels.Word)
-	InsertOrUpdate(wrod datamodels.Word) (updatedWord datamodels.Word, err error)
-	Delete(where string) (deleted bool)
+	Select(where string)(datamodels.Word, bool)
+	SelectMany(where, group_by, order_by string, page, size int) []datamodels.Word
+	InsertOrUpdate(wrod datamodels.Word) ( datamodels.Word,  error)
+	Delete(where string) bool
 }
 
 
@@ -40,3 +40,20 @@ func (r *wordMysqlRepository) Select(where string) (w datamodels.Word, found boo
 	found = true
 	return 
 }
+
+
+func (r *wordMysqlRepository) SelectMany(where, group_by, order_by string, page, size int)  []datamodels.Word{
+	
+}
+
+
+
+func (r *wordMysqlRepository) InsertOrUpdate(wrod datamodels.Word) ( datamodels.Word,  error) {
+	
+}
+
+
+func (r *wordMysqlRepository) SelectMany(where string) bool{
+	
+}
+
